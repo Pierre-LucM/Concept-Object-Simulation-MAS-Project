e2154b91f03a9c208e47617e6b72283c8b79f66e
Pierre-Luc M. <pierre-luc.millet@student.junia.com>
2024-11-18 16:45:13 +0100

    feat() : Add basic implementation of map and refacto prject architecture (#10)
    * change folder architecture and add basic map implementation
    
    * change folder architecture and add basic map implementation

--- SIMULATION_MAS/src/main/java/org/SAPLA/Map/EntityModule.java
+++
@@ -1,9 +0,0 @@
-package org.SAPLA.Map;
-
-import com.google.inject.AbstractModule;
-
-public class EntityModule extends AbstractModule {
-    protected void configure() {
-        bind(IInteractable.class).to(Entity.class);
-    }
-}
