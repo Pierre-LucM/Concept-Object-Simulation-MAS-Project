cba9d3d9a5c00f1ff0a95b3e7fd372b0157f90ce
Arthur Pineau <58333043+Arthur-P0@users.noreply.github.com>
2024-11-19 10:46:58 +0100

    feat() : Add BadBeing, Faction3 and Faction4 (#12)
    * add BadBeing, Faction3 and Faction4
    
    * add getter and setter
    
    * modification

---
+++ src/main/java/org/SAPLA/LivingBeing/BadBeing/Faction3/Faction3.java
@@ -0,0 +1,16 @@
+package org.SAPLA.LivingBeing.BadBeing.Faction3;
+
+import org.SAPLA.LivingBeing.BadBeing.BadBeing;
+
+public class Faction3 extends BadBeing {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void interact() {
+
+    }
+}
---
+++ src/main/java/org/SAPLA/LivingBeing/BadBeing/Faction4/Faction4.java
@@ -0,0 +1,17 @@
+package org.SAPLA.LivingBeing.BadBeing.Faction4;
+import org.SAPLA.LivingBeing.BadBeing.BadBeing;
+
+import java.util.List;
+
+public class Faction4 extends BadBeing {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void interact() {
+
+    }
+}
