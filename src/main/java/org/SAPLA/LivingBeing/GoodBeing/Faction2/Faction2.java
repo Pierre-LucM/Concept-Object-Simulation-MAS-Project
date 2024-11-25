60c31971980cd4b0f1ba8a78d865edbcdf7bc78b
sarah-petit <60597342+sarah-petit@users.noreply.github.com>
2024-11-25 17:24:30 +0100

    feat() : Add GoodBeing, Faction1, Faction2 (#14)

---
+++ src/main/java/org/SAPLA/LivingBeing/GoodBeing/Faction2/Faction2.java
@@ -0,0 +1,15 @@
+package org.SAPLA.LivingBeing.GoodBeing.Faction2;
+
+import org.SAPLA.LivingBeing.GoodBeing.GoodBeing;
+
+public class Faction2 extends GoodBeing {
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void interact() {
+
+    }
+}
