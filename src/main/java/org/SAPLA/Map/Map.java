240306ab22600b6ef0e6fbf3941a78c0095f8448
Pierre-Luc M. <pierre-luc.millet@student.junia.com>
2024-11-25 16:28:03 +0100

    feat() : Basic implementation of some Map method (#13)

--- src/main/java/org/SAPLA/Map/Map.java
+++ src/main/java/org/SAPLA/Map/Map.java
@@ -1,6 +1,7 @@
 package org.SAPLA.Map;
 
 import org.SAPLA.Enum.Direction;
+import org.SAPLA.LivingBeing.LivingBeing;
 
 import java.util.Dictionary;
 
@@ -25,8 +26,53 @@ public class Map {
         return 0;
     }
 
-    public Direction directionToReachSafeZone(Object livingBeing){
-        return null;
+    public Direction directionToReachSafeZone(LivingBeing livingBeing){
+
+        Tile currentTile = livingBeing.getCurrentTile();
+        SafeZone safeZone = livingBeing.getSafeZone();
+        Tile[][] safeZoneGrid = safeZone.getSafeZoneGrid();
+
+        // If the living being is already in the safe zone, return null
+        if (currentTile.isSafeZone()) {
+            return null;
+        }
+
+        // get relative position of the safe zone from the current tile
+        int x = safeZoneGrid[0][0].getPosition().getX() - currentTile.getPosition().getX();
+        int y = safeZoneGrid[0][0].getPosition().getY() - currentTile.getPosition().getY();
+
+        // if the safe zone is in the same row as the current tile
+        if (x == 0) {
+            if (y > 0) {
+                return Direction.NORTH;
+            } else {
+                return Direction.SOUTH;
+            }
+        }
+
+        // if the safe zone is in the same column as the current tile
+        if (y == 0) {
+            if (x > 0) {
+                return Direction.EAST;
+            } else {
+                return Direction.WEST;
+            }
+        }
+
+        // if the safe zone is in the diagonal of the current tile
+        if (x > 0) {
+            if (y > 0) {
+                return Direction.NORTHEAST;
+            } else {
+                return Direction.SOUTHEAST;
+            }
+        } else {
+            if (y > 0) {
+                return Direction.NORTHWEST;
+            } else {
+                return Direction.SOUTHWEST;
+            }
+        }
     }
 
 
